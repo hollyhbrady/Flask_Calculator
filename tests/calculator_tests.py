6ca59249bf462b12f33b3409fc99c635577a2a31
hollyhbrady <hollyhbrady@gmail.com>
2021-04-14 18:51:11 +0100

    adjust tests

--- tests/calculator_tests.py
+++ tests/calculator_tests.py
@@ -3,14 +3,14 @@ from modules import calculator
 
 class TestCalculator(unittest.TestCase):
 
-    def setUp(self):
+    def test_add(self):
         self.assertEqual(5, add(2, 3))
 
-    def setUp(self):
+    def test_subtract(self):
         self.assertEqual(4, subtract(6, 2)) 
 
-    def setUp(self):
+    def test_divide(self):
         self.assertEqual(2, divide(10, 5))
 
-    def setUp(self):
+    def test_multiply(self):
         self.assertEqual(6, multiply(2, 3))
